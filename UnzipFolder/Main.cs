87048bc3433ad3e2b15173b9151ac8ea2e6e09a9
TakeoYamaguchi <gf.yamaguchi@gmail.com>
2020-09-29 10:46:50 +0900

    ドラックアンドドロップに対応する

--- UnzipFolder/Main.cs
+++ UnzipFolder/Main.cs
@@ -1,4 +1,5 @@
 ﻿using System;
+using System.IO;
 using System.Windows.Forms;
 
 namespace UnzipFolder
@@ -8,6 +9,52 @@ namespace UnzipFolder
 		public Main()
 		{
 			InitializeComponent();
+
+			tbInput.AllowDrop = true;
+			tbInput.DragEnter += TbInput_DragEnter;
+			tbInput.DragDrop += TbInput_DragDrop;
+
+			tbOutput.AllowDrop = true;
+			tbOutput.DragEnter += TbInput_DragEnter; //流用する
+			tbOutput.DragDrop += TbOutput_DragDrop;
+
+		}
+
+
+		private void TbInput_DragEnter(object sender, DragEventArgs e)
+		{
+			if (e.Data.GetDataPresent(DataFormats.FileDrop))
+			{
+				e.Effect = DragDropEffects.Copy;
+			}
+			else
+			{
+				e.Effect = DragDropEffects.None;
+			}
+		}
+
+		private void TbInput_DragDrop(object sender, DragEventArgs e)
+		{
+			string[] fileName = (string[])e.Data.GetData(DataFormats.FileDrop, false);
+			if(fileName.Length > 0)
+			{
+				if( File.GetAttributes(fileName[0]).HasFlag(FileAttributes.Directory) )
+				{
+					tbInput.Text = fileName[0];
+				}
+			}
+		}
+
+		private void TbOutput_DragDrop(object sender, DragEventArgs e)
+		{
+			string[] fileName = (string[])e.Data.GetData(DataFormats.FileDrop, false);
+			if (fileName.Length > 0)
+			{
+				if (File.GetAttributes(fileName[0]).HasFlag(FileAttributes.Directory))
+				{
+					tbOutput.Text = fileName[0];
+				}
+			}
 		}
 
 		private void btnInput_Click(object sender, EventArgs e)
